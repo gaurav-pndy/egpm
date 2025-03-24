56978dba34cdd3d14e3e29111a682d6b2d16527a
gaurav-pndy <gauravpndy00@gmail.com>
2025-03-24 11:58:03 +0530

    created about page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,24 +10,32 @@ const Footer = () => {
         <div className="w-12 h-0.5 bg-black mx-auto mb-14" />
 
         <div className="flex flex-wrap max-w-4xl mx-auto justify-around md:justify-between items-center gap-6 md:gap-10 pb-15 px-4 md:px-10">
-          <img
-            src="https://static.wixstatic.com/media/e6f22e_0d85db60e80f4c3dba541969cea6010c~mv2.png"
-            alt="EAFO"
-            className="h-6 md:h-10 object-contain"
-          />
-          <img
-            src="https://static.wixstatic.com/media/e6f22e_52f4d169105b4000b967f882ba858459~mv2.png"
-            alt="ECRC"
-            className="h-10 md:h-17 object-contain"
-          />
-          <img
-            src="https://static.wixstatic.com/media/e6f22e_05679ffe1f6b46fd8dfbdc733c319524~mv2.png"
-            alt="FIRMST"
-            className="h-14 md:h-22 object-contain"
-          />
-          <h4 className="text-xl md:text-2xl font-semibold text-blue-900">
-            Dr. Soma
-          </h4>
+          <a href="https://www.eafo.info/">
+            <img
+              src="https://static.wixstatic.com/media/e6f22e_0d85db60e80f4c3dba541969cea6010c~mv2.png"
+              alt="EAFO"
+              className="h-6 md:h-11 object-contain"
+            />
+          </a>
+          <a href="https://www.ecrc.pro/">
+            <img
+              src="https://static.wixstatic.com/media/e6f22e_52f4d169105b4000b967f882ba858459~mv2.png"
+              alt="ECRC"
+              className="h-10 md:h-17 object-contain"
+            />
+          </a>
+          <a href="https://www.firmst.tech/">
+            <img
+              src="https://static.wixstatic.com/media/e6f22e_05679ffe1f6b46fd8dfbdc733c319524~mv2.png"
+              alt="FIRMST"
+              className="h-14 md:h-22 object-contain"
+            />
+          </a>
+          <a href="https://www.drsoma.info/">
+            <h4 className="text-xl md:text-2xl font-bold text-[rgb(0,37,65)]">
+              Dr. Soma
+            </h4>
+          </a>
         </div>
       </div>
       <div className="text-center  text-[0.9rem]">
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="flex flex-col items-center mb-12">
+      {/* Header with background image */}
+      <div className="relative w-full ">
+        <div className="absolute inset-0 bg-[rgb(0,56,98)]/30"></div>
+        <img
+          src="https://static.wixstatic.com/media/11062b_f71f178b5f494268bf2f5420c4820ae3~mv2.jpg/v1/fill/w_1899,h_319,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/11062b_f71f178b5f494268bf2f5420c4820ae3~mv2.jpg"
+          alt="Mountain landscape background"
+          className="w-full object-cover h-[65vh] md:h-[35vh]"
+        />
+
+        {/* Content overlay */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center p-4 px-2 text-white text-center ">
+          <h2 className="text-4xl md:text-5xl font-bold mb-3">GRASP Goals</h2>
+          <p className="text-xl md:text-4xl mb-2">for</p>{" "}
+          <p className="font-semibold text-xl md:text-4xl">
+            Eurasian Global Peace Mission
+          </p>
+        </div>
+      </div>
+
+      {/* Goals content */}
+      <div className="w-full  mt-3 max-w-5xl text-[rgb(0,37,65)] p-4 md:p-6 text-base md:text-[1.6rem] leading-7 md:leading-8 font-[500]">
+        <h3 className=" font-bold mb-8 ">Our Goals</h3>
+        <div className="space-y-6 ml-8  leading-relaxed">
+          <p>
+            <span className="font-bold underline text-3xl">G</span>uidance and
+            coordination particularly in the field of education
+          </p>
+          <p>
+            <span className="font-bold underline text-3xl">R</span>oad map for
+            resolving individual issues in a coherent and coordinated manner
+          </p>
+          <p>
+            <span className="font-bold underline text-3xl">A</span>
+            ssistance in obtaining official support from respective authorities
+          </p>
+          <p>
+            <span className="font-bold underline text-3xl">S</span>
+            trengthen cultural exchange between nations to promote values
+          </p>
+          <p>
+            <span className="font-bold underline text-3xl">P</span>romotion of
+            holistic model to build an integrated peace movement
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
